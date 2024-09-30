86b1aedff1fb5b96be700ecdeb84921c8269c050
Sagar BK <Sagar.BK1@ibm.com>
2024-09-30 20:44:14 +0530

    Scale images migrated to a different account for support

--- scale-image-map.tf
+++ scale-image-map.tf
@@ -2,16 +2,15 @@
 locals {
   scale_image_region_map = {
     "hpcc-scale5201-rhel88" = {
-      "au-syd"   = "r026-1b434bb0-22ca-4d22-8ab8-5f894e742b03"
-      "br-sao"   = "r042-d60bf44d-544c-406b-9eb7-e8aaa74f11cf"
-      "ca-tor"   = "r038-91f4e2f0-cef5-4e9c-b321-4450f57ec1e3"
-      "eu-de"    = "r010-50ca7e47-2a99-45c5-a8c0-e87df64e55c3"
-      "eu-es"    = "r050-1068a31b-66e9-45af-b689-812c63a32ff9"
-      "eu-gb"    = "r018-cb74b034-7570-47ce-9056-a8a50b1f9c09"
-      "jp-osa"   = "r034-a10f08b9-b82c-470d-8e8c-5cc8abd048d5"
-      "jp-tok"   = "r022-a23c1c17-1f75-4407-98ad-7b55471f5325"
-      "us-east"  = "r014-3bdeef85-499e-479a-bbf5-60f0549a6003"
-      "us-south" = "r006-5bdaae02-1392-4374-8eea-96f964c00adb"
+      "eu-gb"     = "r018-a557df4a-549f-4000-bd4c-56fa8f5512dc"
+      "eu-de"     = "r010-c5dec858-162f-43dd-80b0-6c9bd5add75c"
+      "us-east"   = "r014-1be5173b-cc5a-4381-9f41-05e408268a7d"
+      "us-south"  = "r006-25f14ed9-04c2-4f44-8501-1b44c6a7017d"
+      "jp-tok"    = "r022-26f9f53e-ff97-43dc-a9f3-f2751f5e1ca9"
+      "jp-osa"    = "r034-54eb476b-b3eb-4ab5-9021-b0c28ce6b029"
+      "au-syd"    = "r026-5d849a3c-d5a8-417f-b446-c73448e2dc25"
+      "br-sao"    = "r042-2a70e75b-8096-4935-9807-3481cd40dd67"
+      "ca-tor"    = "r038-d7f70caf-4c95-4faa-8b2f-8f9b9c7f5713"
     }
   }
 }
